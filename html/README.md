6dad18d29985031c58aa2d83074beaeb8c3dd4ae
Timothy Van Heest <timothy@ionicsecurity.com>
2016-02-01 09:19:16 -0500

    First steps toward static site for managing blanket

---
+++ html/README.md
@@ -0,0 +1,23 @@
+# Blanket UI
+
+## Quick start
+
+    # Install
+    npm install
+
+    # Run dev server
+    gulp serve
+
+## Tech stack
+
+* single page angular application served from index.html
+* bootstrap scss
+    * http://getbootstrap.com/css/#sass
+    * https://github.com/twbs/bootstrap-sass#d-npm--nodejs
+    * https://gist.github.com/ericbarnes/ac3ae075c97c1073869c
+* gulp / live reload
+    * https://browsersync.io/
+    * https://browsersync.io/docs/gulp/
+* angular js
+    * https://angularjs.org/
+    * hosted on cdn
---
+++ html/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+    return gulp.src("app/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
