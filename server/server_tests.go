a8641bb95c07db764ad33df5e8c3d94b259e4dc3
Timothy Van Heest <timothy@ionicsecurity.com>
2016-04-02 14:03:45 -0400

    DRYer tests for server

--- server/server_tests.go
+++ server/server_tests.go
@@ -32,46 +32,41 @@ func NewTestServer() (*graceful.Server, func()) {
 	}
 }
 
+// Assert that the request object passed generated an empty list json response
+func assertEmptyListResponse(t *testing.T, req *http.Request) {
+	c := http.Client{}
+	var body []byte
+	var err error
+	var resp *http.Response
+
+	resp, err = c.Do(req)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
+	body, err = ioutil.ReadAll(resp.Body)
+	assert.Equal(t, string(body), "[]")
+	assert.Equal(t, err, nil)
+}
+
 func TestGetTasks(t *testing.T) {
+	// FIXME: Run on random unused port
+	// Run server on port 6777
 	S, closefn := NewTestServer()
 	defer closefn()
 	viper.Set("port", 6777)
 	go S.ListenAndServe()
 	defer S.Stop(time.Millisecond * 100)
 
-	c := http.Client{}
-	var body []byte
-	var err error
-	var resp *http.Response
 	var req *http.Request
 
 	// Tasks from all sources
-	req, err = http.NewRequest("GET", "http://localhost:6777/task", nil)
-	assert.Equal(t, err, nil)
-	resp, err = c.Do(req)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, resp.StatusCode, http.StatusOK)
-	body, err = ioutil.ReadAll(resp.Body)
-	assert.Equal(t, string(body), "[]")
-	assert.Equal(t, err, nil)
+	req, _ = http.NewRequest("GET", "http://localhost:6777/task", nil)
+	assertEmptyListResponse(t, req)
 
 	// Tasks from DB only
-	req, err = http.NewRequest("GET", "http://localhost:6777/task?states=RUNNING", nil)
-	assert.Equal(t, err, nil)
-	resp, err = c.Do(req)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, resp.StatusCode, http.StatusOK)
-	body, err = ioutil.ReadAll(resp.Body)
-	assert.Equal(t, string(body), "[]")
-	assert.Equal(t, err, nil)
+	req, _ = http.NewRequest("GET", "http://localhost:6777/task?states=RUNNING", nil)
+	assertEmptyListResponse(t, req)
 
 	// Tasks from Q only
-	req, err = http.NewRequest("GET", "http://localhost:6777/task?states=WAITING", nil)
-	assert.Equal(t, err, nil)
-	resp, err = c.Do(req)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, resp.StatusCode, http.StatusOK)
-	body, err = ioutil.ReadAll(resp.Body)
-	assert.Equal(t, string(body), "[]")
-	assert.Equal(t, err, nil)
+	req, _ = http.NewRequest("GET", "http://localhost:6777/task?states=WAITING", nil)
+	assertEmptyListResponse(t, req)
 }
